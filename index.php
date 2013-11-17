98e35861beffe59ee32ea1300ca91054f20f9d84
timerepository <bm@Mikhails-MacBook-Pro.local>
2013-11-17 18:20:41 +0100

    Installed Bootstrap template

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+	 <link href="_/css/mystyles.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>comming soon</h1>
+
+   
+    <script src="_/js/mybootstrap.js"></script>
+    <script src="_/js/myscripts.js"></script>
+  </body>
+</html>
